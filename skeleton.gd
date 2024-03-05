4babb8c910a262f383bc22fc6139e7cec0e31270
unknown <bryantdalton19@gmail.com>
2024-03-05 15:54:53 -0500

    skeleton pathing fixed

--- skeleton.gd
+++ skeleton.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
-@onready var farmer_pos = get_node("/root/Game/Farmer")
+@onready var Player_pos = get_node("/root/Game/Player")
 
 func _physics_process(_delta):
-	var direction = global_position.direction_to(farmer_pos.global_position)
-	velocity = direction * 150
-	move_and_slide()
+	var follow_player = global_position.direction_to(Player_pos.global_position)
+	var enemies_in_range = %agro_area.get_overlapping_bodies()
+	var chase_enemy = global_position.direction_to(enemies_in_range.front().global_position)
+	if enemies_in_range.size() > 0:
+		velocity = chase_enemy * 150
+		move_and_slide()
+	else:
+		velocity = follow_player * 150
+		move_and_slide()
